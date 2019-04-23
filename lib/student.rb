a690af324e8c2133a13790d8d78eb6666843381c
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-23 17:21:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    binding.pry
+    # binding.pry
     id = row[0]
     name = row[1]
     grade = row[2]
