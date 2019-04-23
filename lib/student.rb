ede0e2f24ba07bd0faf959070d153de34492b6ba
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-23 18:23:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,6 +67,18 @@ class Student
     DB[:conn].execute(sql, x).map {|row| self.new_from_db(row)}
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      GROUP BY grade ASC
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
