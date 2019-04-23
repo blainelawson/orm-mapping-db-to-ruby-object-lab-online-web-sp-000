517ffaa1515f08d02aabdb485249466211f3dc05
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-23 17:32:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,9 +21,7 @@ class Student
       FROM students
     SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
